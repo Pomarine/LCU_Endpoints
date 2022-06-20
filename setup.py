4e21575168e8828dde0bd945586938d562ebf92d
unknown <mehead888@gmail.com>
2022-06-20 08:30:22 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='LCU_Endpoints',
+    version='0.0.1',
+    author='Pomarine',
+    url='https://github.com/Pomarine/LCU_Endpoints',
+    license='MIT',
+    packages=['LCU_Endpoints'],
+    install_requires=[],
+)
